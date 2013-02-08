11621f0113de47730f01589165c5e61e9f04fcf1
Dmitry <sir.dyf@gmail.com>
2013-02-08 23:10:53 +0600

    clearing code
    P.S. refactoring needed

--- controls.js
+++ controls.js
@@ -2,209 +2,226 @@
  * @author mrdoob / http://mrdoob.com/
  */
 
-THREE.PointerLockControls = function ( camera ) {
+THREE.PointerLockControls = function(camera) {
 
-	var scope = this;
+    var scope = this;
 
-	var yawObject = new THREE.Object3D();
-	//yawObject.position.y = 10;
-	yawObject.add( camera );
-        
-    yawObject.boundRadius=20;//DEBUG
+    var yawObject = new THREE.Object3D();
+    //yawObject.position.y = 10;
+    yawObject.add(camera);
+
+    yawObject.boundRadius = 20;//DEBUG
 
 
-        var moveForward     = false;
-	var moveBackward    = false;
-	var moveLeft        = false;
-	var moveRight       = false;
-	var rotateYawCW     = false; // По часовой
-	var rotateYawСCW    = false; // Против часовой
+    var moveForward = false;
+    var moveBackward = false;
+    var moveLeft = false;
+    var moveRight = false;
+    var rotateYawCW = false; // По часовой
+    var rotateYawСCW = false; // Против часовой
 
-	var isOnObject = false;
-	var canJump    = false;
+    var isOnObject = false;
+    var canJump = false;
 
-	velocity     = new THREE.Vector3();
-	
-        var rotateYaw  = new THREE.Vector3();
-	
-        var velocityYaw  = 0;
+    velocity = new THREE.Vector3();
 
-	var PI_2 = Math.PI / 2;
+    var rotateYaw = new THREE.Vector3();
 
-	var onMouseMove = function ( event ) {
+    var velocityYaw = 0;
 
-		if ( scope.enabled === false ) return;
+    var PI_2 = Math.PI / 2;
 
-		var movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
-		var movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
+    var onMouseMove = function(event) {
 
-		yawObject.rotation.y -= movementX * 0.002;
-		pitchObject.rotation.x -= movementY * 0.002;
+        if (scope.enabled === false)
+            return;
 
-		pitchObject.rotation.x = Math.max( - PI_2, Math.min( PI_2, pitchObject.rotation.x ) );
+        var movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
+        var movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
 
-	};
+        yawObject.rotation.y -= movementX * 0.002;
+        pitchObject.rotation.x -= movementY * 0.002;
 
-	var onKeyDown = function ( event ) {
+        pitchObject.rotation.x = Math.max(-PI_2, Math.min(PI_2, pitchObject.rotation.x));
 
-		switch ( event.keyCode ) {
+    };
 
-			case 38: // up
-			case 87: // w
-				moveForward = true;
-				break;
+    var onKeyDown = function(event) {
 
-			case 37: // left
-				moveLeft = true; break;
-			
-                        case 65: // a
-                                rotateYawСCW = true; break;
+        switch (event.keyCode) {
 
-			case 40: // down
-			case 83: // s
-				moveBackward = true; break;
+            case 38: // up
+            case 87: // w
+                moveForward = true;
+                break;
 
-			case 39: // right
-				moveRight = true; break;
-                                
-			case 68: // d
-                                rotateYawCW = true; break;
+            case 37: // left
+                moveLeft = true;
+                break;
+
+            case 65: // a
+                rotateYawСCW = true;
+                break;
+
+            case 40: // down
+            case 83: // s
+                moveBackward = true;
+                break;
+
+            case 39: // right
+                moveRight = true;
+                break;
+
+            case 68: // d
+                rotateYawCW = true;
+                break;
 
 //			case 32: // space
 //				if ( canJump === true ) velocity.y += 10;
 //				canJump = false;
 //				break;
-                                
-       			case 81: // Q 
-                            break;
-                            
-			case 69: // E 
-                            break;
 
-		}
+            case 81: // Q 
+                break;
 
-	};
+            case 69: // E 
+                break;
 
-	var onKeyUp = function ( event ) {
-
-		switch( event.keyCode ) {
-
-			case 38: // up
-			case 87: // w
-				moveForward = false;
-				break;
-
-			case 37: // left
-                                moveLeft = false;
-				break;
-
-                        case 65: // a
-                                rotateYawСCW = false; 
-                                break;
-
-			case 40: // down
-			case 83: // a
-				moveBackward = false;
-				break;
-
-			case 39: // right
-				moveRight = false;
-				break;
-			
-                        case 68: // d
-                                rotateYawCW = false; 
-				break;
-                                
-       			case 81: // Q 
-                            break;
-                            
-			case 69: // E 
-                            break;                                
-
-		}
-
-	};
-
-	//document.addEventListener( 'mousemove', onMouseMove, false );
-	document.addEventListener( 'keydown', onKeyDown, false );
-	document.addEventListener( 'keyup', onKeyUp, false );
-
-	this.enabled = false;
-
-	this.getObject = function () {
-
-		return yawObject;
-
-	};
-
-	this.isOnObject = function ( boolean ) {
-
-		isOnObject = boolean;
-		canJump = boolean;
-
-	};
-        
-        var impAngleValue=0;
-
-        this.GetVelocity = function(){
-            return velocity;
         }
-   this.SetImpulse = function( angleValue){
-            impAngleValue=angleValue;
+
+    };
+
+    var onKeyUp = function(event) {
+
+        switch (event.keyCode) {
+
+            case 38: // up
+            case 87: // w
+                moveForward = false;
+                break;
+
+            case 37: // left
+                moveLeft = false;
+                break;
+
+            case 65: // a
+                rotateYawСCW = false;
+                break;
+
+            case 40: // down
+            case 83: // a
+                moveBackward = false;
+                break;
+
+            case 39: // right
+                moveRight = false;
+                break;
+
+            case 68: // d
+                rotateYawCW = false;
+                break;
+
+            case 81: // Q 
+                break;
+
+            case 69: // E 
+                break;
+
+        }
+
+    };
+
+    //document.addEventListener( 'mousemove', onMouseMove, false );
+    document.addEventListener('keydown', onKeyDown, false);
+    document.addEventListener('keyup', onKeyUp, false);
+
+    this.enabled = false;
+
+    this.getObject = function() {
+
+        return yawObject;
+
+    };
+
+    this.isOnObject = function(boolean) {
+
+        isOnObject = boolean;
+        canJump = boolean;
+
+    };
+
+    var impAngleValue = 0;
+
+    this.GetVelocity = function() {
+        return velocity;
+    };
+    this.SetImpulse = function(angleValue) {
+        impAngleValue = angleValue;
 
 //************
 //var vector = new THREE.Vector3( 1, 0, 0 );
 
-var axis = new THREE.Vector3( 0, 1, 0 );
+        var axis = new THREE.Vector3(0, 1, 0);
 //var angle = Math.PI / 2;
-var matrixRot = new THREE.Matrix4().makeRotationAxis( axis, angleValue );
+        var matrixRot = new THREE.Matrix4().makeRotationAxis(axis, angleValue);
 
-matrixRot.multiplyVector3( velocity );
+        matrixRot.multiplyVector3(velocity);
 //************
-        };
-        
-    var speedMax=10;//максимальная скорость
-    var accelStop=0.08;//ускорение торможения
-    var accelMove=0.12;//ускорение движения
-    var accelYawMove=0.0016;//ускорение вращения
-    var accelYawStop=0.08;
+    };
+
+    var speedMax = 10;//максимальная скорость
+    var accelStop = 0.08;//ускорение торможения
+    var accelMove = 0.12;//ускорение движения
+    var accelYawMove = 0.0016;//ускорение вращения
+    var accelYawStop = 0.08;
     //var speedYawMax=0.016;
 
-    var yawObject_position_y = 3; 
+    var yawObject_position_y = 3;
 
-    this.update = function ( delta ) {//delta is "delta time"
+    this.update = function(delta) {//delta is "delta time"
 
-            if ( scope.enabled === false ) return;
+        if (scope.enabled === false)
+            return;
 // торможения
-            velocity.x  += ( - velocity.x ) * accelStop * delta; // уменьшение скорости = торможение X
-            velocity.z  += ( - velocity.z ) * accelStop * delta; // уменьшение скорости = торможение Z
-            velocityYaw += (-velocityYaw)*accelYawStop*delta;
+        velocity.x += (-velocity.x) * accelStop * delta; // уменьшение скорости = торможение X
+        velocity.z += (-velocity.z) * accelStop * delta; // уменьшение скорости = торможение Z
+        velocityYaw += (-velocityYaw) * accelYawStop * delta;
 
 // ускорения
-            if ( moveForward ) velocity.z+= accelMove * delta; // увеличение скорости = движение вперёд
-            if ( moveBackward ) velocity.z -= accelMove * delta;
+        if (moveForward)
+            velocity.z += accelMove * delta; // увеличение скорости = движение вперёд
+        if (moveBackward)
+            velocity.z -= accelMove * delta;
 
-            if ( moveLeft ) velocity.x += accelMove * delta;// увеличение скорости = движение влево
-            if ( moveRight ) velocity.x -= accelMove * delta;
+        if (moveLeft)
+            velocity.x += accelMove * delta;// увеличение скорости = движение влево
+        if (moveRight)
+            velocity.x -= accelMove * delta;
 // фактические изменения конечных величин
 
-                rotateYaw.z += ( - rotateYaw.z ) * 0.016 * delta*2;
-                if ( rotateYawCW )  {velocityYaw -= accelYawMove*delta;}
-		if ( rotateYawСCW ) {velocityYaw += accelYawMove*delta;}//rotateYaw.y += velocityYaw;}
-  //         if (Math.abs(velocityYaw)>speedYawMax){
-  //              velocityYaw=speedYawMax*Math.sign(velocityYaw);
-  //          }
-            rotateYaw.y+= velocityYaw;
-                if ( rotateYawCW )  rotateYaw.z += 0.016 * delta / 2;
-		if ( rotateYawСCW ) rotateYaw.z -= 0.016 * delta / 2;
+        rotateYaw.z += (-rotateYaw.z) * 0.016 * delta * 2;
+        if (rotateYawCW) {
+            velocityYaw -= accelYawMove * delta;
+        }
+        if (rotateYawСCW) {
+            velocityYaw += accelYawMove * delta;
+        }//rotateYaw.y += velocityYaw;}
+        //         if (Math.abs(velocityYaw)>speedYawMax){
+        //              velocityYaw=speedYawMax*Math.sign(velocityYaw);
+        //          }
+        rotateYaw.y += velocityYaw;
+        if (rotateYawCW)
+            rotateYaw.z += 0.016 * delta / 2;
+        if (rotateYawСCW)
+            rotateYaw.z -= 0.016 * delta / 2;
 
-            yawObject.rotation.y  = rotateYaw.y;
-            yawObject.rotation.z  = rotateYaw.z;
+        yawObject.rotation.y = rotateYaw.y;
+        yawObject.rotation.z = rotateYaw.z;
 
-            yawObject.translateX( velocity.x );
-            yawObject.translateZ( velocity.z );
-		
-            yawObject.position.y = yawObject_position_y;
-	};
+        yawObject.translateX(velocity.x);
+        yawObject.translateZ(velocity.z);
+
+        yawObject.position.y = yawObject_position_y;
+    };
 
 };
--- init.js
+++ init.js
@@ -1,9 +1,10 @@
-if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
+if (!Detector.webgl)
+    Detector.addGetWebGLMessage();
 
 var SCREEN_WIDTH = window.innerWidth;
 var SCREEN_HEIGHT = window.innerHeight;
 
-var container,stats;
+var container, stats;
 
 var camera, scene, renderer, geometry, terrain, meterial_g;
 
@@ -24,238 +25,228 @@ init();
 animate();
 
 function init() {
-this.init=true;
-        container = document.createElement( 'div' );
-        document.body.appendChild( container );
+    this.init = true;
+    container = document.createElement('div');
+    document.body.appendChild(container);
 
-        renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer = new THREE.WebGLRenderer({antialias: true});
 
-        camera = new THREE.PerspectiveCamera( 35, SCREEN_WIDTH / SCREEN_HEIGHT, 1, 2500 );
-        camera.lookAt(new THREE.Vector3(0,0,1));
+    camera = new THREE.PerspectiveCamera(35, SCREEN_WIDTH / SCREEN_HEIGHT, 1, 2500);
+    camera.lookAt(new THREE.Vector3(0, 0, 1));
 
-        scene = new THREE.Scene();
+    scene = new THREE.Scene();
 
-        PointLight = new pointlight(scene);
+    PointLight = new pointlight(scene);
 
-        scene.add( camera );
-	
-        scene.fog = new THREE.FogExp2( 0x050510, 0.0025 );
-        //scene.fog = new THREE.Fog( 'rgb( 50, 50, 50 )', 1, 700 );
-        //scene.fog.color = new THREE.Color( 20, 20, 20 );
+    scene.add(camera);
 
-        scene.add( new THREE.AmbientLight( 'rgb(5,5,20)' ) );
+    scene.fog = new THREE.FogExp2(0x050510, 0.0025);
+    //scene.fog = new THREE.Fog( 'rgb( 50, 50, 50 )', 1, 700 );
+    //scene.fog.color = new THREE.Color( 20, 20, 20 );
 
-        //scene.add( new THREE.AmbientLight( 0x111111 ) );
+    scene.add(new THREE.AmbientLight('rgb(5,5,20)'));
 
-        directionalLight = new THREE.DirectionalLight( 0xffffff, 1.15 );
-        directionalLight.position.set( 500, 2000, 0 );
-        scene.add( directionalLight );
+    //scene.add( new THREE.AmbientLight( 0x111111 ) );
 
-        var maxAnisotropy = renderer.getMaxAnisotropy();
-        
-        if ( maxAnisotropy > 0 ) {
+    directionalLight = new THREE.DirectionalLight(0xffffff, 1.15);
+    directionalLight.position.set(500, 2000, 0);
+    scene.add(directionalLight);
 
-                document.getElementById( "val_left" ).innerHTML = maxAnisotropy;
+    var maxAnisotropy = renderer.getMaxAnisotropy();
 
-        } else {
+    if (maxAnisotropy > 0) {
 
-                document.getElementById( "val_left" ).innerHTML = "not supported";
-                document.getElementById( "val_right" ).innerHTML =  "not supported";
+        document.getElementById("val_left").innerHTML = maxAnisotropy;
 
-        }
-        
-        terrain = new terrain(scene, maxAnisotropy);
-        
-        var mat = new THREE.MeshLambertMaterial( { color: 0xffffff, shading: THREE.FlatShading, overdraw: true } );
-        sphere = new THREE.Mesh( new THREE.SphereGeometry( 20, 20, 10 ), mat );
-        //sphere.scale.set( 10, 10, 10 );
-        sphere.position.z = 150;
-        sphere.position.x = -50;
-        //sphere.geometry.boundingSphere.radius=20;
-        scene.add( sphere );
-        
-        for(var i=0; i< 20; i++) {
-            
-            loader = new THREE.JSONLoader();
-            loader.load( 'model/fabric.json', function ( geometry ) {
+    } else {
 
-                    meterial_g = new THREE.MeshLambertMaterial( { color: 0xffffff, shading: THREE.FlatShading, overdraw: true } );
+        document.getElementById("val_left").innerHTML = "not supported";
+        document.getElementById("val_right").innerHTML = "not supported";
 
-                    mesh1 = new THREE.Mesh( geometry, meterial_g );
-                    mesh1.position.x = Math.random() * 1000 - 500;
-                    mesh1.position.z = Math.random() * 1000 - 500;
-                    mesh1.geometry.boundingSphere.radius=10;
-                   // scene.add( mesh1 );
+    }
 
-                    mesh2 = new THREE.Mesh( geometry, meterial_g );
-                    mesh2.rotation.x = Math.PI;
-                    mesh2.position.x = mesh1.position.x;
-                    mesh2.position.z = mesh1.position.z;
-                    //mesh1.geometry.boundingSphere.radius=10;
-                    //scene.add( mesh2 );
+    terrain = new terrain(scene, maxAnisotropy);
 
-            } );
-        }
+    var mat = new THREE.MeshLambertMaterial({color: 0xffffff, shading: THREE.FlatShading, overdraw: true});
+    sphere = new THREE.Mesh(new THREE.SphereGeometry(20, 20, 10), mat);
+    //sphere.scale.set( 10, 10, 10 );
+    sphere.position.z = 150;
+    sphere.position.x = -50;
+    //sphere.geometry.boundingSphere.radius=20;
+    scene.add(sphere);
 
-        // RENDERER
+    for (var i = 0; i < 20; i++) {
 
-        renderer.setSize( SCREEN_WIDTH, SCREEN_HEIGHT );
-        renderer.setClearColor( scene.fog.color, 1 );
-        renderer.autoClear = false;
+        loader = new THREE.JSONLoader();
+        loader.load('model/fabric.json', function(geometry) {
 
-        renderer.domElement.style.position = "relative";
-        container.appendChild( renderer.domElement );
+            meterial_g = new THREE.MeshLambertMaterial({color: 0xffffff, shading: THREE.FlatShading, overdraw: true});
 
-        // STATS1
+            mesh1 = new THREE.Mesh(geometry, meterial_g);
+            mesh1.position.x = Math.random() * 1000 - 500;
+            mesh1.position.z = Math.random() * 1000 - 500;
+            mesh1.geometry.boundingSphere.radius = 10;
+            // scene.add( mesh1 );
 
-        stats = new Stats();
-        container.appendChild( stats.domElement );
+            mesh2 = new THREE.Mesh(geometry, meterial_g);
+            mesh2.rotation.x = Math.PI;
+            mesh2.position.x = mesh1.position.x;
+            mesh2.position.z = mesh1.position.z;
+            //mesh1.geometry.boundingSphere.radius=10;
+            //scene.add( mesh2 );
 
-        controls = new THREE.PointerLockControls( camera );
-        scene.add( controls.getObject() );
-        controls.enabled = true;
-        
+        });
+    }
 
-        var renderModel = new THREE.RenderPass( scene, camera );
-        var effectBloom = new THREE.BloomPass( 1.2 );
-        var effectCopy  = new THREE.ShaderPass( THREE.CopyShader );
+    // RENDERER
 
-        effectFXAA = new THREE.ShaderPass( THREE.FXAAShader );
+    renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+    renderer.setClearColor(scene.fog.color, 1);
+    renderer.autoClear = false;
 
-        var width = window.innerWidth || 2;
-        var height = window.innerHeight || 2;
+    renderer.domElement.style.position = "relative";
+    container.appendChild(renderer.domElement);
 
-        effectFXAA.uniforms[ 'resolution' ].value.set( 1 / width, 1 / height );
+    // STATS1
 
-        effectCopy.renderToScreen = true;
+    stats = new Stats();
+    container.appendChild(stats.domElement);
 
-        composer = new THREE.EffectComposer( renderer );
-
-        composer.addPass( renderModel );
-        //composer.addPass( effectFXAA );
-        //composer.addPass( effectBloom );
-        composer.addPass( effectCopy );
+    controls = new THREE.PointerLockControls(camera);
+    scene.add(controls.getObject());
+    controls.enabled = true;
 
 
-//        scene.fire = {
-//            
-//            laser: function(){
-//                myLaser(this,controls.getObject());
-//            }
-//        };
-//    scene.fireLaser = function(){
-//        var tt=new myLaser();
-//        myLaser(this,controls.getObject());
-//    };
-    scene.fireLaser=laser;
-    UTILS.addAll(scene);//DEBUG
-};
+    var renderModel = new THREE.RenderPass(scene, camera);
+    var effectBloom = new THREE.BloomPass(1.2);
+    var effectCopy = new THREE.ShaderPass(THREE.CopyShader);
+
+    effectFXAA = new THREE.ShaderPass(THREE.FXAAShader);
+
+    var width = window.innerWidth || 2;
+    var height = window.innerHeight || 2;
+
+    effectFXAA.uniforms[ 'resolution' ].value.set(1 / width, 1 / height);
+
+    effectCopy.renderToScreen = true;
+
+    composer = new THREE.EffectComposer(renderer);
+
+    composer.addPass(renderModel);
+    //composer.addPass( effectFXAA );
+    //composer.addPass( effectBloom );
+    composer.addPass(effectCopy);
+
+    scene.fireLaser = laser;
+    
+    UTILS.addLine(scene);//DEBUG
+    UTILS.addLine(scene);//DEBUG
+    UTILS.addLine(scene);//DEBUG
+}
+;
 
 
 function animate() {
 
-        requestAnimationFrame( animate );
-        render();
-        stats.update();
+    requestAnimationFrame(animate);
+    render();
+    stats.update();
 
-};
-var vv=0;
-        var sumdeltaD=0;
+}
+;
+var vv = 0;
+var sumdeltaD = 0;
 function render() {
 
-        var delta_time = clock.getDelta();
-        sumdeltaD+=1;
-        renderer.clear();
+    var delta_time = clock.getDelta();
+    sumdeltaD += 1;
+    renderer.clear();
 
-        controls.update( delta_time * 10);
-        terrain.update(controls.getObject());
-        
-        mouseX = 0;
-        
-        for(var index in scene.children) {
-            mouseX += 1;
-            var object = scene.children[index];
-            if (true){//sumdeltaD > 10){
-                CheckCollisionWithCamera(object);
-           }
-            if ("update" in object){
-                object.update( delta_time );
-             }
-         }
-            if (sumdeltaD > 10){
-                sumdeltaD=0;
-            }
+    controls.update(delta_time * 10);
+    terrain.update(controls.getObject());
 
-        //document.getElementById( "val_right" ).innerHTML = sumdelta;
+    mouseX = 0;
 
-        // renderer.render( scene, camera );
-        
-	renderer.clear();
-        composer.render();
+    for (var index in scene.children) {
+        mouseX += 1;
+        var object = scene.children[index];
+        if (true) {//sumdeltaD > 10){
+            CheckCollisionWithCamera(object);
+        }
+        if ("update" in object) {
+            object.update(delta_time);
+        }
+    }
+    if (sumdeltaD > 10) {
+        sumdeltaD = 0;
+    }
 
-	time = Date.now();
+    //document.getElementById( "val_right" ).innerHTML = sumdelta;
+
+    // renderer.render( scene, camera );
+
+    renderer.clear();
+    composer.render();
+
+    time = Date.now();
 
 }
 
-function CheckCollisionWithCamera(obj){
-      
-    
-document.getElementById( "val_right" ).innerHTML = vv;
+function CheckCollisionWithCamera(obj) {
 
-    if (obj.geometry && obj.geometry.boundingSphere.radius<90){// instanceof THREE.Mesh){
-    var camObj = camera.parent;
-    var camRadius=camObj.boundRadius;
-    var objRadius=obj.geometry.boundingSphere.radius;
-    var camPos=camObj.position.clone();
-    var objPos=obj.position.clone(); 
-    var bothRadius=objRadius+camRadius;
-        
-        if (objPos.distanceTo(camPos)<bothRadius){//distanceToSquared//фактическая позиция камеры не меняется!
+
+    document.getElementById("val_right").innerHTML = vv;
+
+    if (obj.geometry && obj.geometry.boundingSphere.radius < 90) {// instanceof THREE.Mesh){
+        var camObj = camera.parent;
+        var camRadius = camObj.boundRadius;
+        var objRadius = obj.geometry.boundingSphere.radius;
+        var camPos = camObj.position.clone();
+        var objPos = obj.position.clone();
+        var bothRadius = objRadius + camRadius;
+
+        if (objPos.distanceTo(camPos) < bothRadius) {//distanceToSquared//фактическая позиция камеры не меняется!
+
+            var vRadius = new THREE.Vector3();
+            vRadius.sub(objPos, camPos);
+            vRadius.y = -1;//чтобы визуально было виднее
+
+            var vel = controls.GetVelocity().clone();
+            if (vel.lengthSq < 1) {//если скорость маленькая, берём направление взгляда
+                vel= new THREE.Vector3(0,0,1);
+            }
+            vel= new THREE.Vector3(0,0,1);//DEBUG
+            vel.normalize();
+
+            var dir = new THREE.Vector3();
+            dir = camObj.localToWorld(vel);//new THREE.Vector3(0, 0, 1));//поправить, чтобы не нормализовать и не прибавлять потом позицию
+            dir.sub(camPos, dir);
+            dir.normalize();
             
-        var vRadius=new THREE.Vector3();
-//        vRadius.sub(camPos,objPos);
-        vRadius.sub(objPos,camPos);
-        vRadius.y=-1;
-        //vRadius.normalize();
-        //vRadius.z *=-1;
-    
-        var dir = new THREE.Vector3();
-        var vel=controls.GetVelocity().clone();
-        if (vel.lengthSq<1){//если скорость маленькая, берём направление взгляда
-//            vel= new THREE.Vector3(0,0,1);
-        }
-        vel.normalize();
-//vel= new THREE.Vector3(0,0,1);
-        dir = camObj.localToWorld(vel);//new THREE.Vector3(0, 0, 1));//поправить, чтобы не нормализовать и не прибавлять потом позицию
-        dir.sub(camPos,dir);
-       
-        dir.normalize();
-        var vRadiusNorm=vRadius.clone();
-        
-        vRadiusNorm.normalize();
-        var alpha=dir.angleTo(vRadiusNorm);        
-        
-vv = alpha*57;//dir.dot(vRadiusNorm);
+            var vRadiusNorm = vRadius.clone();
+            vRadiusNorm.normalize();
+            
+            var alpha = dir.angleTo(vRadiusNorm);
 
-UTILS.lookTo(camPos,vRadius);
-UTILS.lookToDir(camPos,dir);
-UTILS.lookTo2(camPos,dir);
-//UTILS.line2.rotation.copy(UTILS.line1.rotation);
-var delta=dir.subSelf(dir);
-delta=camObj.worldToLocal(delta);
-var sign=delta.x>0 ? 1:-1;
-UTILS.line2.rotation.y = 2*alpha*sign;
-vv=sign;
-//UTILS.line2.position=camPos.clone();
-        //obj.position.addSelf(camera.parent.position);
+            UTILS.lookTo(0,camPos, vRadius);
+            UTILS.lookTo(1,camPos, dir);
+            UTILS.lookTo(2,camPos, dir);
+
+            var delta = dir.subSelf(dir);
+            delta = camObj.worldToLocal(delta);
+            var sign = delta.x > 0 ? 1 : -1;
+            
+            UTILS.lines[2].rotation.y = 2 * alpha * sign;
+//            vv = sign;//DEBUG
+
 //        if (dir.dot(vRadiusNorm)>0){
 //            controls.SetImpulse(180-2*alpha);
-            camera.parent.position.x=0;
-            camera.parent.position.y=0;
-            camera.parent.position.z=0;
+            camera.parent.position.x = 0;
+            camera.parent.position.y = 0;
+            camera.parent.position.z = 0;
 //        }
         }
-        
+
     }
 }
 
@@ -264,21 +255,21 @@ vv=sign;
 //{ min: new THREE.Vector3(), max: new THREE.Vector3() }
 //.boundingSphere
 
-var onKeyDownMain = function ( event ) {
-    if (event.keyCode === 32){ // Пробел
-        
+var onKeyDownMain = function(event) {
+    if (event.keyCode === 32) { // Пробел
+
         //scene.fire.laser();
         //
         scene.fireLaser(controls.getObject());
-        
+
         // meterial_g.needsUpdate = true;
-        
+
         //for(l in PoingLight){
         //    PoingLight[l].distance = 0;
         //    PoingLight[l].enabled = false;
         //}
-    
+
     }
 };
 
-document.addEventListener( 'keydown', onKeyDownMain, false );
+document.addEventListener('keydown', onKeyDownMain, false);
